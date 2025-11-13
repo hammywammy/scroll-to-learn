903665c9b862679defa0d22e6bfb0240fbb8cf6f
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-11-13 12:16:00 -0800

    Create useAppStore.ts

---
+++ mobile/store/useAppStore.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+interface AppState {
+  isAuthenticated: boolean;
+  currentVideoIndex: number;
+  setAuthenticated: (value: boolean) => void;
+  setCurrentVideo: (index: number) => void;
+}
+
+export const useAppStore = create<AppState>((set) => ({
+  isAuthenticated: false,
+  currentVideoIndex: 0,
+  setAuthenticated: (value) => set({ isAuthenticated: value }),
+  setCurrentVideo: (index) => set({ currentVideoIndex: index }),
+}));
